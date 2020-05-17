914cbf6c86336d6e1001a7d100b7a44ffe11c86a
Marcus Sanatan <msanatan@gmail.com>
2020-05-17 00:13:03 -0400

    Create Prealoder scene
    This scene will be run at the beginning to load all the assets.
    There aren't much assets right now but the levels should add a few.
    Once the fonts are loaded we move to the game scene.
    If the wait time gets longer we'll add a loader to it.
    
    The key constants are in their own file as well. Easier to manage

---
+++ src/scenes/PreloadScene.js
@@ -0,0 +1,39 @@
+import Phaser from 'phaser';
+import { DOOR_KEY, PLAYER_KEY, TILEMAP_KEY, TILES_KEY } from './keys';
+
+export default class PreloadScene extends Phaser.Scene {
+  constructor() {
+    super('preload-scene');
+  }
+
+  preload() {
+    this.load.spritesheet(
+      PLAYER_KEY,
+      'assets/images/cuadrado.png',
+      {
+        frameWidth: 32,
+        frameHeight: 32
+      }
+    );
+    this.load.image(TILES_KEY, 'assets/images/tiles.png');
+    this.load.image(DOOR_KEY, 'assets/images/door.png');
+    this.load.tilemapTiledJSON(TILEMAP_KEY, 'assets/tilemaps/level1.json');
+    // Load Google Font script
+    this.load.script('webfont',
+      'https://ajax.googleapis.com/ajax/libs/webfont/1.6.26/webfont.js');
+  }
+
+  create() {
+    // Load fonts
+    WebFont.load({
+      custom: {
+        families: ['Pixel Inversions'],
+        urls: ['../fonts.css']
+      },
+      active: () => {
+        console.log('Fonts loaded');
+        this.scene.start('game-scene');
+      }
+    });
+  }
+}
---
+++ src/scenes/keys.js
@@ -0,0 +1,4 @@
+export const PLAYER_KEY = 'player';
+export const DOOR_KEY = 'door';
+export const TILES_KEY = 'tiles';
+export const TILEMAP_KEY = 'tilemap';
