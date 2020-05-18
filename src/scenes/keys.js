76fd5cf846963fb5deea73a0dcb4eede70f4af10
Marcus Sanatan <msanatan@gmail.com>
2020-05-18 19:52:43 -0400

    Add a function to generate tilemap keys
    We'll load a few stages so this is best to keep it consistent

--- src/scenes/keys.js
+++ src/scenes/keys.js
@@ -1,5 +1,13 @@
 export const PLAYER_KEY = 'player';
 export const DOOR_KEY = 'door';
 export const TILES_KEY = 'tiles';
-export const TILEMAP_KEY = 'tilemap';
 export const BACKGROUND_KEY = 'background';
+
+/**
+ * Generates a key for levels
+ * @param {number} levelNum - Level identifier
+ * @return {string}
+ */
+export const getLevelKey = (levelNum) => {
+  return `tilemap_level_${levelNum}`;
+}
