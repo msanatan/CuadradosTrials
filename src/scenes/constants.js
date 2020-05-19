70eae52361e02b7b3cd16588b9a11e5e36bffa0f
Marcus Sanatan <msanatan@gmail.com>
2020-05-18 22:53:55 -0400

    Make constants for Tiled IDs
    Better than having strings repeated or looking like magic

--- src/scenes/constants.js
+++ src/scenes/constants.js
@@ -11,3 +11,12 @@ export const BACKGROUND_KEY = 'background';
 export const getLevelKey = (levelNum) => {
   return `tilemap_level_${levelNum}`;
 }
+
+export const TILED_EXIT_DOOR_LAYER = 'ExitDoor';
+export const TILED_PLATFORMS_LAYER = 'Platforms';
+export const TILED_MOVING_PLATFORMS_LAYER = 'MovingPlatforms';
+export const TILED_SPIKES_LAYER = 'Spikes';
+
+export const TILED_DOOR_KEY = 'Door';
+export const TILED_HORIZONTAL_MOVING_PLATFORM_KEY = 'Horizontal';
+export const TILED_VERTICAL_MOVING_PLATFORM_KEY = 'Vertical';
