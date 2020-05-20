cb7adc4c47c7a1477005d757429a8ad748ff9c30
Marcus Sanatan <msanatan@gmail.com>
2020-05-20 00:03:09 -0400

    Move level creation into it's own function
    We need to refactor like this so that we can play multiple levels

--- src/scenes/GameScene.js
+++ src/scenes/GameScene.js
@@ -11,46 +11,22 @@ export default class GameScene extends Phaser.Scene {
      */
     this.player = null;
     /**
-     * @type {Phaser.Tilemaps.Tilemap}
+     * @type {number}
      */
-    this.level = null;
+    this.level = 1;
   }
 
   create() {
-    // Add Tiled level
-    this.level = this.make.tilemap({ key: getLevelKey(1) });
-    // Add background image
-    const background = this.add.image(
-      this.level.widthInPixels / 2,
-      this.level.heightInPixels / 2,
-      BACKGROUND_KEY
-    );
-    let scaleX = this.level.widthInPixels / background.width;
-    let scaleY = this.level.heightInPixels / background.height;
-    let scale = Math.max(scaleX, scaleY);
-    background.setScale(scale).setScrollFactor(0);
-
-    // Display Tiled level
-    const tileset = this.level.addTilesetImage('Cuadrado\'s Tiles', TILES_KEY);
-    const platforms = this.level.createStaticLayer('Platforms', tileset, 0, 0);
-    platforms.setCollisionByExclusion(-1, true);
-
-    // Add door
-    const [door] = this.level.createFromObjects(
-      TILED_EXIT_DOOR_LAYER, TILED_DOOR_KEY, { key: DOOR_KEY }, this);
-    this.physics.world.enable(door, Phaser.Physics.Arcade.DYNAMIC_BODY);
-    door.body.setImmovable(true);
-    door.body.allowGravity = false;
-    door.setOrigin(0.5, 0.5);
-
+    // Setup level
+    const [tilemap, platforms, door] = this.setupLevel(this.level);
     // Create player
     this.player = this.createPlayer(50, 656);
     // Setup collisions with world
     this.player.setCollideWorldBounds(true);
     this.physics.world.checkCollision.up = false;
     this.physics.world.checkCollision.down = false;
-    this.physics.world.bounds.width = this.level.widthInPixels;
-    this.physics.world.bounds.height = this.level.heightInPixels;
+    this.physics.world.bounds.width = tilemap.widthInPixels;
+    this.physics.world.bounds.height = tilemap.heightInPixels;
     // Setup collisions with platform tiles
     this.physics.add.collider(this.player, platforms);
     // Setup collisions with exit door
@@ -61,7 +37,7 @@ export default class GameScene extends Phaser.Scene {
     this.cursors = this.input.keyboard.createCursorKeys();
 
     // Setup camera
-    this.cameras.main.setBounds(0, 0, this.level.widthInPixels, this.level.heightInPixels);
+    this.cameras.main.setBounds(0, 0, tilemap.widthInPixels, tilemap.heightInPixels);
     this.cameras.main.startFollow(this.player);
   }
 
@@ -70,7 +46,7 @@ export default class GameScene extends Phaser.Scene {
       return;
     }
 
-    if (this.player.y > this.level.heightInPixels) {
+    if (this.player.y > this.physics.world.bounds.height) {
       this.playerReset(50, 656);
       return;
     }
@@ -139,6 +115,36 @@ export default class GameScene extends Phaser.Scene {
     };
 
     return player;
+  }
+
+  setupLevel(level) {
+    // Add Tiled level
+    const tilemap = this.make.tilemap({ key: getLevelKey(level) });
+    // Add background image
+    const background = this.add.image(
+      tilemap.widthInPixels / 2,
+      tilemap.heightInPixels / 2,
+      BACKGROUND_KEY
+    );
+    let scaleX = tilemap.widthInPixels / background.width;
+    let scaleY = tilemap.heightInPixels / background.height;
+    let scale = Math.max(scaleX, scaleY);
+    background.setScale(scale).setScrollFactor(0);
+
+    // Display Tiled level
+    const tileset = tilemap.addTilesetImage('Cuadrado\'s Tiles', TILES_KEY);
+    const platforms = tilemap.createStaticLayer('Platforms', tileset, 0, 0);
+    platforms.setCollisionByExclusion(-1, true);
+
+    // Add door
+    const [door] = tilemap.createFromObjects(
+      TILED_EXIT_DOOR_LAYER, TILED_DOOR_KEY, { key: DOOR_KEY }, this);
+    this.physics.world.enable(door, Phaser.Physics.Arcade.DYNAMIC_BODY);
+    door.body.setImmovable(true);
+    door.body.allowGravity = false;
+    door.setOrigin(0.5, 0.5);
+
+    return [tilemap, platforms, door]
   }
 
   /**
