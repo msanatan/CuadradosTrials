ba88b6008dcead567d020c3b88a4ac5e4c559836
Marcus Sanatan <msanatan@gmail.com>
2020-05-24 17:18:35 -0400

    Remove delta and tween logic from platforms
    We'll be using velocity and collisions instead

--- src/entities/MovingPlatform.js
+++ src/entities/MovingPlatform.js
@@ -15,30 +15,6 @@ export const createMovingPlatform = (platform, scene) => {
   platform.startX = platform.x;
   platform.startY = platform.y;
 
-  // Add variables for deltas, we'll use them to update the player's values
-  // so they can ride on the platforms
-  platform.deltaX = 0;
-  platform.deltaY = 0;
-  platform._previousX = platform.x;
-  platform._previousY = platform.y;
-
-  // Override preUpdate function to calculate deltas
-  platform.preUpdate = function () {
-    this.deltaX = this.x - this._previousX;
-    this.deltaY = this.y - this._previousY;
-    this._previousX = this.x;
-    this._previousY = this.y;
-  };
-
-  // Add some helper functions so I don't have to work with data properties
-  platform.getDestinationX = function () {
-    return platform.startX + platform.data.list[1].value * TILE_SIZE;
-  };
-
-  platform.getDestinationY = function () {
-    return platform.startY + platform.data.list[3].value * TILE_SIZE;
-  };
-
   platform.getHold = function () {
     return platform.data.list[0].value;
   };
@@ -51,29 +27,18 @@ export const createMovingPlatform = (platform, scene) => {
     return platform.data.list[4].value;
   };
 
-  platform.getDuration = function () {
-    switch (platform.name.toLowerCase()) {
-      case 'horizontal':
-        return Math.abs(platform.getSpeedX() * platform.data.list[1].value);
-      case 'vertical':
-        return Math.abs(platform.getSpeedY() * platform.data.list[3].value);
-    }
+  platform.toggleSpeed = function () {
+    platform.data.list[2].value = -platform.data.list[2].value;
+    platform.data.list[4].value = -platform.data.list[4].value;
   };
+
+  // Add property for when it hit boundary
+  platform.justHitBoundary = false;
 
   // Configure physics
   scene.physics.world.enable(platform, Phaser.Physics.Arcade.DYNAMIC_BODY);
-  platform.body.setFriction(1, 1);
+  platform.body.setFrictionX(1);
   platform.body.setImmovable(true);
   platform.body.setAllowGravity(false);
-  scene.add.tween({
-    targets: platform,
-    x: platform.getDestinationX(),
-    y: platform.getDestinationY(),
-    duration: platform.getDuration(),
-    yoyo: true,
-    repeat: -1,
-    delay: platform.getHold(), // Initial pause before firing
-    repeatDelay: platform.getHold(), // Pause when the tween yoyos (i.e. comes back to original spot)
-    hold: platform.getHold(), // Pause when tween reaches destination
-  });
+  platform.body.setVelocity(platform.getSpeedX(), platform.getSpeedY());
 };
