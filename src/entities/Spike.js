837790858328b5feb4c9fac252fca1cb7d371737
Marcus Sanatan <msanatan@gmail.com>
2020-05-28 00:07:06 -0400

    Update spike image so black line won't appear
    Hopefully this takes care of the bug like it did for Cuadradro's image.

--- src/entities/Spike.js
+++ src/entities/Spike.js
@@ -6,6 +6,7 @@ import Phaser from 'phaser';
  * @param {Phaser.Scene} scene
  */
 export const createSpike = (spike, scene) => {
+  spike.setSize(32, 32);
   spike.setOrigin(0.5, 0.5);
   scene.physics.world.enable(spike, Phaser.Physics.Arcade.DYNAMIC_BODY);
   spike.body.setImmovable(true);
