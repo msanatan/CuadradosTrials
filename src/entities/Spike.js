d83e77acd2ff2c9507cc21a167d6c7928e7fdf56
Marcus Sanatan <msanatan@gmail.com>
2020-05-28 21:36:11 -0400

    Reduce height of spike
    It's a bit unfair for the player to collide with whitespace

--- src/entities/Spike.js
+++ src/entities/Spike.js
@@ -6,9 +6,9 @@ import Phaser from 'phaser';
  * @param {Phaser.Scene} scene
  */
 export const createSpike = (spike, scene) => {
-  spike.setSize(32, 32);
   spike.setOrigin(0.5, 0.5);
   scene.physics.world.enable(spike, Phaser.Physics.Arcade.DYNAMIC_BODY);
+  spike.body.setSize(32, 30);
   spike.body.setImmovable(true);
   spike.body.setAllowGravity(false);
 };
