f060cb0553ce21cc01d83377ddc55e86897a6493
Marcus Sanatan <msanatan@gmail.com>
2020-05-30 23:20:06 -0400

    Add loader for assets
    I'll add a few more assets to make it worthwhile...

--- src/scenes/PreloadScene.js
+++ src/scenes/PreloadScene.js
@@ -23,6 +23,50 @@ export default class PreloadScene extends Phaser.Scene {
   }
 
   preload() {
+    // Add progress bars
+    const progressBar = this.add.graphics();
+    const progressBox = this.add.graphics();
+    progressBox.fillStyle(0x222222, 0.8);
+    progressBox.fillRect(240, 270, 320, 50);
+
+    const width = this.cameras.main.width;
+    const height = this.cameras.main.height;
+    const loadingText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 50,
+      text: 'LOADING',
+      style: {
+        font: '32px "Courier New"',
+        fill: '#ffffff',
+      },
+    });
+    loadingText.setOrigin(0.5, 0.5);
+
+    const percentText = this.make.text({
+      x: width / 2,
+      y: height / 2 - 5,
+      text: '0%',
+      style: {
+        font: '18px monospace',
+        fill: '#ffffff',
+      },
+    });
+    percentText.setOrigin(0.5, 0.5);
+
+    this.load.on('progress', function (value) {
+      percentText.setText(parseInt(String(value * 100)) + '%');
+      progressBar.clear();
+      progressBar.fillStyle(0xffffff, 1);
+      progressBar.fillRect(250, 280, 300 * value, 30);
+    });
+
+    this.load.on('complete', function () {
+      progressBar.destroy();
+      progressBox.destroy();
+      loadingText.destroy();
+      percentText.destroy();
+    });
+
     this.load.spritesheet(PLAYER_KEY, 'assets/images/cuadrado.png', {
       frameWidth: 32,
       frameHeight: 32,
