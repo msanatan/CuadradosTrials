ccb16455a8b88345cb4aae3404ac636d57bb341d
Marcus Sanatan <msanatan@gmail.com>
2020-05-31 16:30:22 -0400

    The HUD now displays time remaining and coins collected
    Move the text to the right as levels have less clutter there

--- src/scenes/HUDScene.js
+++ src/scenes/HUDScene.js
@@ -8,26 +8,79 @@ import Phaser from 'phaser';
 export default class HUDScene extends Phaser.Scene {
   constructor() {
     super('hud-scene');
+    /**
+     * @type {Phaser.GameObjects.Text}
+     */
+    this.timeRemainingText = null;
+
+    /**
+     * @type {Phaser.GameObjects.Text}
+     */
+    this.coinsRetrievedText = null;
   }
 
   create() {
-    this.textElements = new Map([
-      [
-        'TIME',
-        this.add
-          .text(20, 20, `Time: ${this.registry.get('timeRemaining')}`, {
-            fontFamily: 'Courier, Monaco, monospace',
-            fontSize: '24px',
-            color: '#FFB871',
-          })
-          .setOrigin(0, 0),
-      ],
-    ]);
+    const hudTextConfig = {
+      fontFamily: 'VCR OSD Mono',
+      fontSize: '18px',
+      color: '#FFFFFF',
+      stroke: '#000000',
+      strokeThickness: 1,
+    };
 
+    // Display time remaining text
+    this.timeRemainingText = this.add.text(
+      680,
+      20,
+      this.getTimeReaminingText(this.registry.get('timeRemaining')),
+      hudTextConfig
+    );
+    this.timeRemainingText.setOrigin(0, 0);
+
+    // Update time remaining text
     this.registry.events.on('changedata-timeRemaining', this.updateTime, this);
+
+    // Display time remaining text
+    this.coinsRetrievedText = this.add.text(
+      640,
+      50,
+      this.getCoinsRetrievedText(
+        this.registry.get('coinsCollected'),
+        this.registry.get('totalCoins')
+      ),
+      hudTextConfig
+    );
+    this.coinsRetrievedText.setOrigin(0, 0);
+
+    // Update coins collected text
+    this.registry.events.on('changedata-coinsCollected', this.updateCoinsCollected, this);
+  }
+
+  /**
+   * @param {number} timeRemaining
+   */
+  getTimeReaminingText(timeRemaining) {
+    return `TIME: ${timeRemaining}`;
+  }
+
+  /**
+   * @param {number} coinsCollected
+   * @param {number} totalCoins
+   */
+  getCoinsRetrievedText(coinsCollected, totalCoins) {
+    return `COINS: ${coinsCollected} / ${totalCoins}`;
   }
 
   updateTime() {
-    this.textElements.get('TIME').setText(`Time: ${this.registry.get('timeRemaining')}`);
+    this.timeRemainingText.setText(this.getTimeReaminingText(this.registry.get('timeRemaining')));
+  }
+
+  updateCoinsCollected() {
+    this.coinsRetrievedText.setText(
+      this.getCoinsRetrievedText(
+        this.registry.get('coinsCollected'),
+        this.registry.get('totalCoins')
+      )
+    );
   }
 }
